7b8664ee6475b2b0d9b9a80deb9194717a02e8da
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2022-11-08 10:15:05 +0000

    Tuesday morning worked example

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const yargs = require('yargs');
+const Movie = require("./utils")
 
 const input = yargs.argv;
-// console.log(yargs.argv.actor);
-// console.log(yargs.argv.filmTitle);
 
-if (input.actor) {
-    console.log(`${input.actor} is in the film ${input.filmTitle}`)
+function app(yargsObject) {
+    if (yargsObject.add) {
+        const newMovie = new Movie(yargsObject.title, yargsObject.actor)
+        newMovie.add();
+    } else if (yargsObject.multiadd) {
+        const newMovie1 = new Movie(yargsObject.title1, yargsObject.actor1)
+        newMovie1.add();
+        const newMovie2 = new Movie(yargsObject.title2, yargsObject.actor2)
+        newMovie2.add();
+        const newMovie3 = new Movie(yargsObject.title3, yargsObject.actor3)
+        newMovie3.add();
+    }
 }
+
+app(input);
